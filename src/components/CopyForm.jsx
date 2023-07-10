682f24b95992c40b068699195ca16aa66b68fce0
themms <maximusnewera@mail.ru>
2023-07-10 19:00:48 +0600

    modified:   src/components/CopyForm.jsx

--- src/components/CopyForm.jsx
+++ src/components/CopyForm.jsx
@@ -36,8 +36,7 @@ function CopyForm({ takeForm, setValue }) {
               {index + 1}. {form.text}
               <button
                 type="text"
-                className="break-words justify-end items-end text-end"
-              >
+                className="break-words justify-end items-end text-end">
                 {form.value}
 
               </button>
