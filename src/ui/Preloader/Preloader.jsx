70133d3ba5db116472e90343c5525993356eb4ba
themms <maximusnewera@mail.ru>
2023-11-11 02:20:46 +0600

    modified:   src/ui/Preloader/Preloader.jsx

--- src/ui/Preloader/Preloader.jsx
+++ src/ui/Preloader/Preloader.jsx
@@ -6,7 +6,6 @@ export const Preloader = () => {
       <div className="flex text-6xl animate-pulse justify-center w-1/4 h-1/6 items-center rounded-full">
         Загрузка
       </div>
-
     </>
   );
 };
